84e58ca40250ecefa122a470c054f50d2dd25abf
romria <ryabokon.roman@gmail.com>
2023-06-22 15:03:17 +0300

    reworked error page

--- src/routes/not-found-404.tsx
+++ src/routes/not-found-404.tsx
@@ -3,29 +3,22 @@ import {useRouteError, isRouteErrorResponse, Link} from 'react-router-dom';
 
 export default function ErrorPage(): ReactElement {
   const error = useRouteError();
+  let errorMessage: string;
+
+  if (isRouteErrorResponse(error)) {
+    errorMessage = `${error.status} - ${error.statusText}. "${error.data?.message}"`;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === 'string') {
+    errorMessage = error;
+  } else {
+    errorMessage = 'Unknown error';
+  }
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      {(() => {
-        if (isRouteErrorResponse(error)) {
-          return (
-            <>
-              <p>{error.status} - {error.statusText}</p>
-              {error.data?.message && (
-                <p>
-                  <i>{error.data.message}</i>
-                </p>
-              )}
-            </>
-          );
-        } if (error instanceof Error) {
-          return <p><i>{error.message}</i></p>;
-        } if (typeof error === 'string') {
-          return <p><i>{error}</i></p>;
-        }
-        return <p><i>Unknown error</i></p>;
-      })()}
+    <div>
+      <h1>Oops! Something went wrong...</h1>
+      <p><i>{errorMessage}</i></p>
       <br />
       <Link to="/">Back to Home Page</Link>
     </div>
