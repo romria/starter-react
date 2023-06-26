201c10faac1bf74c3a7346d1290f70107a74fb97
romria <ryabokon.roman@gmail.com>
2023-06-26 18:16:14 +0300

    reworked fetch controller

--- app/templates/src/config/index.ts
+++ app/templates/src/config/index.ts
@@ -1,6 +1,8 @@
-export const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:8080';
+export const API_BASE_URL = 'https://10.255.255.1';
 export const REQUEST_HEADERS = {
   // 'Access-Control-Request-Headers': 'access-control-allow-origin',
   'Accept': 'application/json',
   'Content-Type': 'application/json; charset=UTF-8',
 };
+
+export const REQUEST_TIMEOUT = 8000; // 8 seconds
--- app/templates/src/controllers/request/index.ts
+++ app/templates/src/controllers/request/index.ts
@@ -1,30 +1,70 @@
-import qs from 'qs';
-import {API_BASE_URL, REQUEST_HEADERS} from '../../config';
+// import qs from 'qs';
+import {API_BASE_URL, REQUEST_HEADERS, REQUEST_TIMEOUT} from '../../config';
 
-type RequestParams = {
-  url?: string;
-  headers?: HeadersInit;
-  method?: string;
-  body?: Record<string, any>;
+type KeyValuePairs = Record<string, string>;
+interface RequestParams {
+  url?: string
+  headers?: HeadersInit
+  method?: string
+  body?: KeyValuePairs
 }
 
-const request = (params: RequestParams): Promise<any> => {
+// Recursive objects are not supported here
+const qsStringify = (body: KeyValuePairs): string => new URLSearchParams(body).toString();
+// If you need recursive objects/params then install 'qs' lib
+// const qsStringifyRecursive = (body: KeyValuePairs): string => qs.stringify(body);
+
+const request = async <T>(params: RequestParams): Promise<{data?: Promise<T>, errors?: string[]}> => {
   const {
     url = API_BASE_URL,
     headers = REQUEST_HEADERS,
     method = 'GET',
     body = undefined,
   } = params;
-
-  return fetch(url, {
+  const controller = new AbortController();
+  const init = {
+    signal: controller.signal,
     method,
     headers,
-    ...(body ? method === 'GET' ? {body: qs.stringify(body)} : {body: JSON.stringify(body)} : {}),
-  }).then(response => response.json());
+    ...((body != null) ? method === 'GET' ? {body: qsStringify(body)} : {body: JSON.stringify(body)} : {}),
+  };
+
+  setTimeout((): void => { controller.abort(); }, REQUEST_TIMEOUT);
+
+  try {
+    const response = await fetch(url, init);
+    if (!response.ok) {
+      throw new Error(`HTTP status ${response.status}`);
+    }
+
+    const json = await response.json() as Promise<T>;
+
+    return {data: json};
+  } catch (error) {
+    const errors = [];
+
+    if (error instanceof SyntaxError) {
+      errors.push(`Parsing response failed: ${error.message}`);
+    } else if (error instanceof DOMException && error.name === 'AbortError') {
+      errors.push('Request timed out');
+    } else if (error instanceof Error) {
+      errors.push(`Request failed: ${error.message}`);
+    } else {
+      errors.push(`Request failed: ${JSON.stringify(error)}`);
+    }
+
+    return {errors};
+  }
 };
 
-export const submitData = (body: any): Promise<any> => request({
+export const getData = async<T> (body?: KeyValuePairs): Promise<{data?: Promise<T>, errors?: string[]}> => request({
+  url: `${API_BASE_URL}/data`,
+  method: 'GET',
+  body,
+});
+
+export const submitData = async<T> (body?: KeyValuePairs): Promise<{data?: Promise<T>, errors?: string[]}> => request({
   url: `${API_BASE_URL}/submit`,
   method: 'POST',
   body,
-}).then(({data, errors}) => ({data, errors}));
+});
