214d7195047f6b80f2938145b14567c5bdb172b5
Roman Riabokon <ryabokon.roman@gmail.com>
2023-07-09 21:22:08 +0300

    revert

---
+++ app/templates/src/routes/index/index.tsx
@@ -0,0 +1,11 @@
+import React, {type ReactElement} from 'react';
+
+import classes from './index.scss';
+
+const Index = (): ReactElement => (
+  <div className={classes.index}>
+    <h1>React App Generator</h1>
+  </div>
+);
+
+export default Index;
