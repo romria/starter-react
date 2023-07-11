47b9d5abc73c912011d75cd6a3169e6892d65454
Roman Riabokon <ryabokon.roman@gmail.com>
2023-07-11 19:53:56 +0300

    cleanup

--- app/templates/src/declaration.d.ts
+++ app/templates/src/declaration.d.ts
@@ -1,7 +1,5 @@
 declare module '*.css';
 declare module '*.scss';
-declare module '*.module.css';
-declare module '*.module.scss';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.png';
